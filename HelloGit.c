e58fe15c9f986a3139a32359303b20ba3a557dc7
Jakub-Radinn <Jakub@radinn.com>
2017-08-09 15:57:25 +0200

    kkk
    kkk

---
+++ HelloGit.c
@@ -0,0 +1,13 @@
+
+
+	pc.printf("\n----SPI Message received!----\n");
+            memcpy(&statusMessage, spiRxBuffer, sizeof(StatusMessage));
+
+            pc.printf("remainingBattery: %d\n", statusMessage.remainingBattery);
+
+            throttleMessage.throttle  = throttle;
+            throttleMessage.rssiRx    = rssiRx;
+
+            pc.printf("sent: %d, %d\n", throttleMessage.throttle, throttleMessage.rssiRx);
+            memcpy(spiTxBuffer, &throttleMessage, sizeof(ThrottleMessage));
+            spiSlave.write(spiTxBuffer, SPI_BUFFER_SIZE);
